9afb48168f8b6005cb9d8810778089356b88bc17
DoctorGoat <thearcy@gmail.com>
2020-12-14 15:10:23 -0500

    Add files via upload
    New version

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Universal_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.targetFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backupTargetFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dummy1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadTemplateToolStripMenuItem,
+            this.targetFolderToolStripMenuItem,
+            this.backupTargetFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadTemplateToolStripMenuItem
+            // 
+            this.loadTemplateToolStripMenuItem.Name = "loadTemplateToolStripMenuItem";
+            this.loadTemplateToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.loadTemplateToolStripMenuItem.Text = "Load Template";
+            this.loadTemplateToolStripMenuItem.Click += new System.EventHandler(this.loadTemplateToolStripMenuItem_Click);
+            // 
+            // targetFolderToolStripMenuItem
+            // 
+            this.targetFolderToolStripMenuItem.Name = "targetFolderToolStripMenuItem";
+            this.targetFolderToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.targetFolderToolStripMenuItem.Text = "Target Folder";
+            this.targetFolderToolStripMenuItem.Click += new System.EventHandler(this.targetFolderToolStripMenuItem_Click);
+            // 
+            // backupTargetFolderToolStripMenuItem
+            // 
+            this.backupTargetFolderToolStripMenuItem.Name = "backupTargetFolderToolStripMenuItem";
+            this.backupTargetFolderToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.backupTargetFolderToolStripMenuItem.Text = "Backup Target Folder";
+            this.backupTargetFolderToolStripMenuItem.Click += new System.EventHandler(this.backupTargetFolderToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 454);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 184);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dummy1
+            // 
+            this.dummy1.Location = new System.Drawing.Point(13, 28);
+            this.dummy1.Multiline = true;
+            this.dummy1.Name = "dummy1";
+            this.dummy1.Size = new System.Drawing.Size(776, 35);
+            this.dummy1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(274, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(515, 381);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 650);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dummy1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadTemplateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem targetFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backupTargetFolderToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox dummy1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Universal_Launcher
+{
+    public partial class Form1 : Form
+    {
+        //We want it to default to this folder.
+        public string selectedDirectory = Environment.CurrentDirectory;
+        //standard backup folder deal; this could easily be chosen by the user but i think timestamping is a valuable way to do this
+        static public string backupFolder = Environment.CurrentDirectory + "\\update_backup\\" + DateTime.Now.Ticks + "\\";
+        TextBox selectedFolder = new TextBox();
+        static TextBox console = new TextBox();
+        static StreamWriter sw = new StreamWriter("log.log");
+        static Button button1;
+        private static Boolean hasLoadedTemplate = true;
+
+        public Form1()
+        {
+            //we make a new text box for the selected folder because dealing with textboxes being set at runtime is mildly irritating
+            createFolderText();
+            createConsoleOut();
+            createGameButton();
+            InitializeComponent();
+        }
+
+        //Current folder should be updated as needed
+        private void createFolderText()
+        {
+            selectedFolder.Text = "Current Folder:";
+            selectedFolder.Location = new Point(13, 28);
+            selectedFolder.Multiline = true;
+            selectedFolder.Size = new Size(776, 35);
+            selectedFolder.ReadOnly = true;
+            selectedFolder.AppendText(Environment.NewLine);
+            selectedFolder.AppendText(selectedDirectory);
+            this.Controls.Add(selectedFolder);
+        }
+
+        private void createConsoleOut()
+        {
+            console.Text = "";
+            console.Location = new Point(12, 454);
+            console.Multiline = true;
+            console.Size = new Size(776, 184);
+            console.ReadOnly = true;
+            this.Controls.Add(console);
+        }
+
+        private void createGameButton()
+        {
+            //todo
+            button1.Text = "";
+            button1.Location = new Point(13, 70);
+            button1.Size = new Size(255, 23);
+            button1.Click += new System.EventHandler(gameButton_Click());
+        }
+
+        private EventHandler gameButton_Click()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Selects and overwrites the default folder oF "this folder"
+        private void targetFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            selectedDirectory = fbd.ShowDialog().ToString();
+            selectedFolder.Text = "Current Folder: \n" + selectedDirectory;
+        }
+
+        //takes current folder and puts it in this folder + update_backup + timestamp
+        private void backupTargetFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                //basic output
+                console.AppendText("Backing up current folder...");
+                console.AppendText("Folder name: " + backupFolder);
+                Directory.CreateDirectory(backupFolder);
+                DirectoryInfo targetDir = new DirectoryInfo(backupFolder);
+                DirectoryInfo sourceDir = new DirectoryInfo(selectedDirectory);
+                CopyAll(sourceDir, targetDir);
+        }
+
+        //this is from MSDN
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                console.AppendText(@"Copying " + target.FullName + fi.Name);
+                sw.WriteLine(@"Copying " + target.FullName + fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                if (diSourceSubDir.Name != "update_backup")
+                {
+                    DirectoryInfo nextTargetSubDir =
+                        target.CreateSubdirectory(diSourceSubDir.Name);
+                    CopyAll(diSourceSubDir, nextTargetSubDir);
+                }
+            }
+        }
+        
+        //just neatly closes everything
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sw.Close();
+            this.Close();
+        }
+
+        //ooooohhhhh booy this is where all the json loading code is
+        private void loadTemplateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            hasLoadedTemplate = true;
+        }
+
+        //return filename for actual installation using json information
+        //both of these two functions use some similar ideas: url.split.last gives the filename of a url unless it's some absolutely
+        //cursed bullshit, both use a client with a header so it's not rejected as a unknown client
+
+        //Regex ideally always gets exactly one match. If not, the regex is flawed. The pages are always ones with one match, such as 
+        //https://github.com/cataclysmbnteam/Cataclysm-BN/releases/latest
+        private string githubGet(string url, string regex)
+        {
+            string fileName = "";
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("user-agent", "Goat Updater");
+                Regex rx = new Regex(regex);
+                string githubPage = client.DownloadString(url);
+                MatchCollection matches = rx.Matches(githubPage);
+                fileName = matches[0].ToString().Split('/').Last();
+                client.DownloadFile(@"https://github.com/" + matches[0], matches[0].ToString().Split('/').Last());
+                console.AppendText("Downloaded" + url.Split('/').Last());
+            }
+            return fileName;
+        }
+
+        //return filename for actual installation using json information
+        //honestly kind of tempted to combine this and githubGet
+        private string urlGet(string url)
+        {
+            string fileName = "";
+            using (var client = new WebClient())
+            {
+                //gets filename from URL
+                client.Headers.Add("user-agent", "Goat Updater");
+                client.DownloadFile(url, url.Split('/').Last());
+                console.AppendText("Downloaded" + url.Split('/').Last()); 
+                fileName = url.Split('/').Last();
+            }
+            return fileName;
+        }
+    }
+}
